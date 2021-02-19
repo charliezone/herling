85f292822e6090f259a7e81972fcd7a89c905d90
Carlos Rafael <carlitos051186@gmail.com>
2021-02-19 14:30:48 -0500

    create proyects page

---
+++ wp-content/themes/herling/page-proyectos.php
@@ -0,0 +1,55 @@
+<?php get_header() ?>
+    <!-- Primary Page Layout
+    ================================================== -->
+    <div id="videos">
+        <section class="section main padding-page-top padding-bottom over-hide background-dark-2 d-flex align-items-center">
+            <div class="container">
+                <div class="row video-container">
+                    <div class="col-md-6 d-flex flex-column justify-content-center align-items-start">
+                        <h1>Paradisus Real State</h1>
+                        <small class="mt-3 mb-md-0 mb-3">Venta de las más exclusivas propiedades en Quintana Roo, Yucatán y Campeche</small>
+                    </div>
+                    <div class="col-md-6">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/proyectos/paradisus-logo.jpg" >
+                    </div>
+                </div>
+                <div class="row video-container flex-md-row-reverse">
+                    <div class="col-md-6 d-flex flex-column justify-content-center align-items-start">
+                        <h1>RaRo</h1>
+                        <small class="mt-3 mb-md-0 mb-3">Agencia multimedia y servicios de marketing, reestructuración y representación de artistas.</small>
+                    </div>
+                    <div class="col-md-6">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/proyectos/raro-logo.jpg" >
+                    </div>
+                </div>
+                <div class="row video-container">
+                    <div class="col-md-6 d-flex flex-column justify-content-center align-items-start">
+                        <h1>RaRo Marketing</h1>
+                        <small class="mt-3 mb-md-0 mb-3">División de marketing de la agencia RaRo</small>
+                    </div>
+                    <div class="col-md-6">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/proyectos/raro-marketing-logo.jpg" >
+                    </div>
+                </div>
+                <div class="row video-container flex-md-row-reverse">
+                    <div class="col-md-6 d-flex flex-column justify-content-center align-items-start">
+                        <h1>17th Company</h1>
+                        <small class="mt-3 mb-md-0 mb-3">Gestión de alojamientos</small>
+                    </div>
+                    <div class="col-md-6">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/proyectos/17-company-cover.jpg" >
+                    </div>
+                </div>
+                <div class="row video-container">
+                    <div class="col-md-6 d-flex flex-column justify-content-center align-items-start">
+                        <h1>Ron Ache</h1>
+                        <small class="mt-3 mb-md-0 mb-3">Marca de ron artesanal.</small>
+                    </div>
+                    <div class="col-md-6">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/proyectos/ashe.png" >
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+<?php get_footer() ?>
